fd74598b5bb2842cb0775825d3603c43f4dc0dfb
lekhavan <91652143+lekhavan@users.noreply.github.com>
2021-10-21 08:29:18 +0700

    repair pg

--- DienMayDo/connect.php
+++ DienMayDo/connect.php
@@ -1,5 +1,9 @@
 <?php
-	$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
-	mysqli_query($Connect,'SET NAMES "utf8"');
+	//$Connect = mysqli_connect("localhost","root","","qlbh") or die("Lỗi".mysqli_error($Connect));
+	//mysqli_query($Connect,'SET NAMES "utf8"');
 	//mysqli_close($Connect);
+	$Connect = pg_connect("postgres://eazlcisnrkhcxl:9f53ac5bdffd1a7d15b6241cf644835010941041357401951db4dd0b2f508449@ec2-52-201-72-91.compute-1.amazonaws.com:5432/dhm6m6enahrfl");
+	if (!$Connect) {
+        die("Connection failed");
+    }
 ?>
